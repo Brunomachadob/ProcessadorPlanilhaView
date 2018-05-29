9858e2737899e57221f53b82047d295189399141
Bruno Machado Brandão <brunobrandao@softbox.com.br>
2018-05-29 19:23:04 -0300

    Adicionando loading-bar a aplicação

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { loadProgressBar } from 'axios-progress-bar'
 
+import 'axios-progress-bar/dist/nprogress.css';
 import './index.css';
 
 import App from './app/App';
+
+loadProgressBar();
 
 ReactDOM.render(<App/>, document.getElementById('root'));
